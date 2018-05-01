a209ebba18bac107ff02ecfeb8e6243181c00a25
borodinchik <borodin.vladymyr@gmail.com>
2018-05-01 09:49:01 +0300

    Create Task -a & TaskResource, TaskRequest

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Task;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory(Task::class, 100)->create();
+    }
+}
