2e2897ad81bed7716c8330e4675a7f393ec078d0
borodinchik <borodin.vladymyr@gmail.com>
2018-07-25 16:25:10 +0300

    Change login facebooka

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -91,7 +91,7 @@ class TaskController extends Controller
 
     public function getTasksAndComments(Task $task)
     {
-        $taskComments = Task::with('comments')->where('id', $task->id)->first();
+        $taskComments = Task::with(['comments'])->where('id', $task->id)->first();
         dd($taskComments);
         return $taskComments;
     }
