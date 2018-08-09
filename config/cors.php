f7b6336bc96e0d297da5dcfaba132fd08f43e096
borodinchik <borodin.vladymyr@gmail.com>
2018-08-09 12:24:38 +0300

    CHANGE CORS

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['http://localhost:3000'],
+    'allowedOrigins' => ['*'],
     'allowedHeaders' => ['Content-Type', 'Authorization', 'Accept'],
     'allowedMethods' => ['GET', 'POST', 'PUT', 'DELETE'],
     'exposedHeaders' => [],
